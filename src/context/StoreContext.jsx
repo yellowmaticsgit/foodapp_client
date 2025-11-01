09e5b0d32e86eb2e56c462c42aa61201162e264e
Yellowmatics Git <yellowmaticsgit@gmail.com>
2025-11-01 11:14:26 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'http://localhost:4000';
+  const url = 'https://foodapp-server-6kzh.onrender.com:4000';
   const [token, setToken] = useState('');
   const [food_list, setFoodList] = useState([]);
 
